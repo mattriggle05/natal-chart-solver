4bd5a53ced872fd5dc5f2a9b0564467bf912cbc8
mattriggle05 <mattriggle05@gmail.com>
2026-02-15 16:34:36 -0600

    Convert solar system to show positions by date

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { useState, useMemo } from 'react';
 import SolarSystem from './SolarSystem';
 import './App.css';
-import { Ecliptic, Body, GeoVector } from 'astronomy-engine';
 
 
 function App() {
 
-  const [currBody, setCurrBody] = useState<Body>(Body.Neptune);
   const [currDate, setCurrDate] = useState<string>('2026-01-01');
-
-  var result = useMemo(() => Ecliptic(GeoVector(currBody, new Date(currDate), true)).elon, [currBody,currDate])
 
   return (
     <>
@@ -18,17 +14,10 @@ function App() {
       </div>
 
       <div className="system-container">
-        <SolarSystem />
+        <SolarSystem date={ currDate } />
       </div>
       
-
-      <select value={currBody} onChange={e => setCurrBody(e.target.value as Body)}>
-        {Object.values(Body).map(x => <option key={ x } value ={ x }>{ x }</option>)}
-      </select>
-
       <input type="date" value={currDate} onChange={e => setCurrDate(e.target.value)} />
-
-      <p style={{color: 'white'}}>{result}</p>
     </>
   );
 }
--- src/SolarSystem.tsx
+++ src/SolarSystem.tsx
@@ -1,12 +1,34 @@
+import { useState, useMemo } from 'react';
 import './SolarSystem.css';
+import { Body, EclipticLongitude } from 'astronomy-engine';
 
-function SolarSystem() { 
+
+function SolarSystem({ date }: {date: string}) { 
     
-    const planets = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune']
-    
+    const planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'] as const;
+
+    const [planetPositions, setPlanetPositions] = useState(
+        Object.fromEntries(planets.map(p => [p, 0]))
+    );
+
+    useMemo(() => {
+        const dateObj = new Date(date);
+        setPlanetPositions(
+            Object.fromEntries(
+                planets.map(p => [p, EclipticLongitude(Body[p as keyof typeof Body], dateObj)])
+            )
+        );
+    }, [date]);
+
     return <div className="system">
         <div className="sun"></div>
-        {planets.map(x => <div key={x} className={`orbit ${x}`}><div className={`planet ${x}`}></div></div>)}
+        {Object.keys(planetPositions).map(x =>
+            <div key={x} className={`orbit ${x}`} style={{
+                transform: `translate(-50%,-50%) rotate(${planetPositions[x as keyof typeof planetPositions]}deg)`
+            }}>
+                <div className={`planet ${x}`} />
+            </div>
+        )}
     </div>;
 }
 
