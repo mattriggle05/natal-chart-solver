b4060e9f93cd3384175c71a1cf25f761d670e0ee
mattriggle05 <mattriggle05@gmail.com>
2026-02-15 03:01:10 -0600

    Testing with astronomy-engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-//import React from 'react';
+// import React from 'react';
 import SolarSystem from './SolarSystem';
 import './App.css';
+import { EclipticLongitude, Body, FlexibleDateTime } from 'astronomy-engine';
 
 function App() {
+
+  var result: number = EclipticLongitude(Body.Neptune, new Date(2026,1,1))
+
   return (
     <>
-      
       <div className='description'>
         <h1>Coming soon...</h1>
       </div>
 
       <SolarSystem />
 
+      <select>
+        {Object.values(Body).map(x => <option>{x}</option>)}
+      </select>
+
+      <p>{ result }</p>
     </>
   );
 }
