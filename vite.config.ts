2aa700e08c82c74673749d508b39b2fd17aeeed8
mattriggle05 <mattriggle05@gmail.com>
2026-02-17 22:31:41 -0600

    Configure settings for rust + wasm and add build step

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/natal-chart-solver/',
   build: {
     outDir: 'build',
   },
+  resolve: {
+    alias: {
+      '@wasm': path.resolve(__dirname, 'wasm/pkg')
+    }
+  },
+  worker: {
+    format: 'es'
+  },
+  optimizeDeps: {
+    exclude: ['natal-chart-solver']
+  }
 })
