d2a343b42db3fadca96c56b2fe917199fdae9710
mattriggle05 <mattriggle05@gmail.com>
2026-02-19 20:07:50 -0600

    Set up enum for astrology signs

--- src/types/features.ts
+++ src/types/features.ts
@@ -1,11 +1,10 @@
-// src/types/planets.ts
 export enum Feature {
-  Mercury = 0,
-  Venus   = 1,
-  Earth   = 2,
-  Mars    = 3,
-  Jupiter = 4,
-  Saturn  = 5,
-  Uranus  = 6,
-  Neptune = 7,
+    Mercury = 0,
+    Venus = 1,
+    Earth = 2,
+    Mars = 3,
+    Jupiter = 4,
+    Saturn = 5,
+    Uranus = 6,
+    Neptune = 7,
 }
