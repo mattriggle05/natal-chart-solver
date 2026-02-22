839651f906352e0c49183f91d755f7ce7f65ff17
mattriggle05 <mattriggle05@gmail.com>
2026-02-22 03:35:16 -0600

    working on new search function pt 2

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,9 +1,16 @@
 use wasm_bindgen::prelude::*;
 use vsop87::*;
 
-
 #[wasm_bindgen(start)]
 pub fn main() {}
+
+
+
+
+
+
+
+
 
 
 
@@ -12,36 +19,43 @@ pub fn main() {}
 pub fn search2(start_julian_date: f64, end_julian_date: f64, feature_ids: &[u8], feature_signs: &[u8]) -> Vec<f64> {
     let mut prev_windows: Vec<(f64, f64)> = Vec::from([(start_julian_date, end_julian_date)]);
     let mut curr_windows: Vec<(f64, f64)> = Vec::new();
-    const COARSE_STEP: f64 = 1.0;
+    const DIVIDE_BY_30: f64 = 1.0_f64 / 30.0_f64; // calculated at compile time
 
-    for id in feature_ids.iter() {
+    for i in 0..feature_ids.len() {
+        const COARSE_STEP: f64 = 1.0;
+
         for window in prev_windows.iter() {
             let mut curr_date: f64 = window.0;
-            while curr_date < end_julian_date {
-                let earth_coords: RectangularCoordinates = vsop87c::earth(curr_date);
+            let mut prev_longitude: f64 = 0.0;
+            let mut prev_longitude_valid: bool = false;
+            let mut prev_derivative_sign: i8 = 0;
+            let mut curr_window_start: f64 = -1.0;
 
-                let longitude: f64 = match *id {
-                    //find_windows_planets(&mut curr_windows, start_julian_date, end_julian_date, &vsop87c::jupiter)
-                    // planets
-                    0 => observer_longitude(earth_coords, vsop87c::mercury(curr_date)),
-                    1 => observer_longitude(earth_coords, vsop87c::venus(curr_date)),
-                    // 2 is earth
-                    3 => observer_longitude(earth_coords, vsop87c::mars(curr_date)),
-                    4 => observer_longitude(earth_coords, vsop87c::jupiter(curr_date)),
-                    5 => observer_longitude(earth_coords, vsop87c::saturn(curr_date)),
-                    6 => observer_longitude(earth_coords, vsop87c::uranus(curr_date)),
-                    7 => observer_longitude(earth_coords, vsop87c::neptune(curr_date)),
-                    // sun
-                    10 => (vsop87b::earth(curr_date).longitude().to_degrees() + 180.0).rem_euclid(360.0),
 
-                    // catch all error case
-                    _ => return Vec::from([-1.0]),
-                };
+
+
+            while curr_date < end_julian_date - COARSE_STEP {
+                let curr_longitude: f64 = geocentric_longitude(curr_date, feature_ids[i]);
+                let curr_longitude_valid: bool = (curr_longitude * DIVIDE_BY_30) as u8 == feature_signs[i];
+
+                if !prev_longitude_valid && curr_longitude_valid {
+                    curr_window_start = bisect(curr_date - COARSE_STEP, curr_date, (feature_signs[i] as f64)*30.0, feature_ids[i])
+                } else if prev_longitude_valid && !curr_longitude_valid {
+                    curr_windows.push((curr_window_start, bisect(curr_date - COARSE_STEP, curr_date, ((feature_signs[i]+1) as f64)*30.0, feature_ids[i])))
+                }
+
+                prev_longitude = curr_longitude;
+                prev_longitude_valid = curr_longitude_valid;
 
                 curr_date += COARSE_STEP;
             }
+
+            
+
+
+
         }
-        
+
         prev_windows = curr_windows;
         curr_windows = Vec::new();
     }
@@ -52,21 +66,63 @@ pub fn search2(start_julian_date: f64, end_julian_date: f64, feature_ids: &[u8],
 }
 
 
-// pub fn find_windows_planets(output_vec: &mut Vec<(f64, f64)>, start_julian_date: f64, end_julian_date: f64, longitude_func: fn(&f64) -> ) {
-//     let mut curr_date: f64 = start_julian_date;
-//     const step: f64 = 1.0;
-//     let mut prev_longitude: f64 = 0.0;
-
-//     while curr_date < end_julian_date {
-
-//     }
-
-// }
 
 
-// pub fn step_size_by_planet(feature_id: u8) -> f64 {
-//     return 1.0
-// }
+pub fn bisect(start_julian_date: f64, end_julian_date: f64, target_value: f64, feature_id: u8) -> f64 {
+    let mut left: f64 = start_julian_date;
+    let mut right: f64 = end_julian_date;
+    const FIVE_MINUTES: f64 = 1.0_f64 / 244.0_f64;
+
+    while left <= right {
+        let midpoint: f64 = (left + right) * 0.5;
+        let midpoint_longitude: f64 = geocentric_longitude(midpoint, feature_id);
+
+        if (midpoint_longitude - target_value).abs() < FIVE_MINUTES {
+            return midpoint_longitude
+        } else if midpoint_longitude < target_value {
+            right = midpoint;
+         } else {
+            left = midpoint_longitude
+         }
+    }
+
+    return -1.0
+}
+
+
+
+
+#[inline]
+pub fn geocentric_longitude(julian_date: f64, feature_id: u8) -> f64 {
+    return match feature_id {
+        // planets
+        0 => observer_longitude(vsop87c::earth(julian_date), vsop87c::mercury(julian_date)),
+        1 => observer_longitude(vsop87c::earth(julian_date), vsop87c::venus(julian_date)),
+        // 2 is earth
+        3 => observer_longitude(vsop87c::earth(julian_date), vsop87c::mars(julian_date)),
+        4 => observer_longitude(vsop87c::earth(julian_date), vsop87c::jupiter(julian_date)),
+        5 => observer_longitude(vsop87c::earth(julian_date), vsop87c::saturn(julian_date)),
+        6 => observer_longitude(vsop87c::earth(julian_date), vsop87c::uranus(julian_date)),
+        7 => observer_longitude(vsop87c::earth(julian_date), vsop87c::neptune(julian_date)),
+        // sun
+        10 => (vsop87b::earth(julian_date).longitude().to_degrees() + 180.0).rem_euclid(360.0),
+        // catch all error case
+        _ => return -1.0,
+    };
+}
+
+
+/// Returns the ecliptic longitude of a feature around a specified observer feature
+/// 
+/// * `observer_coords` - RectangularCoordinates of the feature that is the reference frame of the calculation
+/// * `feature_coords` - RectangularCoordinates of the feature whose longitude you want to get
+#[inline]
+pub fn observer_longitude(observer_coords: RectangularCoordinates, feature_coords: RectangularCoordinates) -> f64 {
+    return (feature_coords.y - observer_coords.y).atan2(feature_coords.x - observer_coords.x).to_degrees().rem_euclid(360.0);
+}
+
+
+
 
 /// A function to search through a range of dates and return subsets of those dates where a certain planetary alignment occurred. 
 /// 
@@ -94,7 +150,6 @@ pub fn search(start_julian_date: f64, end_julian_date: f64, feature_ids: &[u8], 
 
     return output;
 }
-
 
 /// Returns true if there exists a valid alignment of the nth feature_id with the nth feature_sign at an exact Julian Date, otherwise false.
 /// 
@@ -127,17 +182,6 @@ pub fn is_valid_date(julian_date: f64, feature_ids: &[u8], feature_signs: &[u8])
 
     return true
 }
-
-
-/// Returns the ecliptic longitude of a feature around a specified observer feature
-/// 
-/// * `observer_coords` - RectangularCoordinates of the feature that is the reference frame of the calculation
-/// * `feature_coords` - RectangularCoordinates of the feature whose longitude you want to get
-#[inline]
-pub fn observer_longitude(observer_coords: RectangularCoordinates, feature_coords: RectangularCoordinates) -> f64 {
-    return (feature_coords.y - observer_coords.y).atan2(feature_coords.x - observer_coords.x).to_degrees().rem_euclid(360.0);
-}
-
 
 /// Returns a list of the solar system's planet's ecliptic longitudes at a given date, used to model the system simply in UI
 /// 
